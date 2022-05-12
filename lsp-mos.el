2cbb4bc5b9215d2e9e11bef127fe778a0cdcbd88
Marie Katrine Ekeberg <mke@themkat.net>
2022-05-12 22:52:53 +0200

    Start lsp mode automatically on mos-mode, and reorder to make everything run on the first eval-buffer

--- lsp-mos.el
+++ lsp-mos.el
@@ -18,28 +18,7 @@
   "Major mode for use with the MOS toolkit for 6502 processors")
 
 (add-to-list 'lsp-language-id-configuration '(mos-mode . "mos"))
-
-(lsp-register-client
- (make-lsp-client
-  :new-connection (lsp-stdio-connection (lambda () (list lsp-mos-executable-path "lsp")))
-  :activation-fn (lsp-activate-on "mos")
-  :major-modes '(mos-mode)
-  :priority -1
-  :server-id 'mos-ls
-  :action-handlers (ht ("mos.runSingleTest" (mos-debug-test t))
-                       ("mos.debugSingleTest" (mos-debug-test nil)))))
-
-;; TODO: possibility to download mos automatically if not found?
-
-(defun lsp-mos-populate-debug-args (conf)
-  (-> conf
-      (dap--put-if-absent :type "mos")
-      (dap--put-if-absent :request "launch")
-      (dap--put-if-absent :workspace (lsp-workspace-root))
-      (dap--put-if-absent :host "localhost")
-      (dap--put-if-absent :debugServer 6503)))
-
-(dap-register-debug-provider "mos" #'lsp-mos-populate-debug-args)
+(add-to-list 'mos-mode-hook #'lsp)
 
 ;; todo: dry!
 (defun mos-build ()
@@ -61,6 +40,28 @@
                         :testRunner (list :testCaseName test-name)
                         :noDebug ,no-debug)))))
 ;; TODO: maybe some sort of support if we don't have lenses active?
+
+(lsp-register-client
+ (make-lsp-client
+  :new-connection (lsp-stdio-connection (lambda () (list lsp-mos-executable-path "-v" "lsp")))
+  :activation-fn (lsp-activate-on "mos")
+  :major-modes '(mos-mode)
+  :priority -1
+  :server-id 'mos-ls
+  :action-handlers (ht ("mos.runSingleTest" (mos-debug-test t))
+                       ("mos.debugSingleTest" (mos-debug-test nil)))))
+
+;; TODO: possibility to download mos automatically if not found?
+
+(defun lsp-mos-populate-debug-args (conf)
+  (-> conf
+      (dap--put-if-absent :type "mos")
+      (dap--put-if-absent :request "launch")
+      (dap--put-if-absent :workspace (lsp-workspace-root))
+      (dap--put-if-absent :host "127.0.0.1")
+      (dap--put-if-absent :debugServer 6503)))
+
+(dap-register-debug-provider "mos" #'lsp-mos-populate-debug-args)
 
 (provide 'lsp-mos)
 ;;; lsp-mos.el ends here
